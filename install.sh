e8257a7b1785f4b3e65228f0364e028b3e2852ad
kenjisubagja <panjisubagja62@gmail.com>
2025-07-29 15:21:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo "=== Octra Pre-Client Setup ==="
 echo "By : Kenji subagja"
 
-# Clone repo dan masuk ke dalamnya
+# Clone repo 
 git clone https://github.com/octra-labs/octra_pre_client.git || exit 1
 cd octra_pre_client || exit 1
 
@@ -12,14 +12,14 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-# Salin wallet contoh
+# 
 cp wallet.json.example wallet.json
 
-# Input PRIVATE KEY dan ADDRESS
+# Input PRIVATE KEY and ADDRESS
 read -p "Enter your PRIVATE KEY B64 : " priv
 read -p "Enter your OCTRA ADDRESS   : " addr
 
-# Buat wallet.json aman tanpa sed
+# submit wallet
 cat > wallet.json <<EOF
 {
   "priv": "$priv",
